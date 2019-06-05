73dfe264da585355596211e958791e35a46e7f49
unknown <stucka@whitedoggies.com>
2019-06-05 14:46:36 -0400

    Don't download files at least XX days newer than their file date

--- gettornadoes.py
+++ gettornadoes.py
@@ -4,21 +4,27 @@ from tqdm import tqdm
 import datetime
 import csv
 import glob
+from time import sleep
 import os
 
+naptime = 0.2   # How many seconds to sleep between downloads?
+MaxDaysOld = 30   # Re-download every file newer than XX days old
 
 # https://www.spc.noaa.gov/climo/reports/190521_rpts_filtered.csv
-# Tornadoes come first. So if row['Time'] = "Time", we're looking at the next type of report and need to break.
+# Tornadoes come first in the file. So if row['Time'] = "Time", we're looking at the next type of report and need to break.
+# We're not attempting to parse out hail and other report types.
 baseurlpre = "https://www.spc.noaa.gov/climo/reports/"
 # baseurlpost = "_rpts_torn.csv"
 baseurlpost = "_rpts_filtered.csv"
 compositecsv = "composite.csv"
 filepre = "Daily"
 datadir = "data/"
-startdateraw = "2019-05-01"
+startdateraw = "2012-01-01"
 enddateraw = "Today"   # YYYY-mm-dd or "Today"
 
 os.makedirs(datadir, exist_ok=True)
+
+timenow  = datetime.datetime.now()   # Don't alter.
 
 if enddateraw == "Today":
     enddate = datetime.datetime.now()
@@ -32,12 +38,19 @@ for i in tqdm(range(0, deltadays + 1)):
     targetdate = startdate + datetime.timedelta(days=i)
     targetfilename = datadir + filepre + targetdate.strftime("%Y-%m-%d.csv")
     targeturl = baseurlpre + targetdate.strftime("%y%m%d") + baseurlpost
-    with open(targetfilename, "wb") as f:
+    WantFile = True
+    if os.path.exists(targetfilename):
+        filedatestamp = datetime.datetime.fromtimestamp(os.path.getmtime(targetfilename))
+        if ((filedatestamp - targetdate).days + 2) > MaxDaysOld:
+            WantFile = False
+    if WantFile:
         r = requests.get(targeturl)
         if r.status_code == 200:   # If the page is good
-            f.write(r.content)
+            with open(targetfilename, "wb") as f:
+                f.write(r.content)
         else:
             print(f"Error retrieving file from {targeturl}")
+        sleep(naptime)
 
 headers = None
 sourcecsvs = list(glob.glob(datadir + filepre + "*.csv"))
